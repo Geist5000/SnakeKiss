51c8b2af85141ea6531ebde7215e89d99c6b344f
Geist5000 <geist5000@gmail.com>
2021-01-28 13:52:29 +0100

    initial commit

---
+++ SnakeKISS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeKISS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SnakeKISS/SnakeGame/Game.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeKISS.SnakeGame
+{
+    class Game
+    {
+    }
+}
